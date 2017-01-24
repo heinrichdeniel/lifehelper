5fe816af2caf1583afce0b607a7dcb6172c96644
Heinrich Deniel <denielheinrich@gmail.com>
2017-01-24 16:25:48 +0200

    webpack updated

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,7 +16,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: 'bundle.js'
   },
   devServer: {
     contentBase: './public',
