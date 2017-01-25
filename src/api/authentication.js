e779fb0b3882a3394d5039655454847996e38d6b
Heinrich Deniel <denielheinrich@gmail.com>
2017-01-25 16:47:23 +0200

    host changed

--- src/api/authentication.js
+++ src/api/authentication.js
@@ -3,7 +3,7 @@ import request from "superagent";
 export function login (credentials) {
   return new Promise((resolve, reject) => {
     request
-      .post('https://lifehelper.herokuapp.com/auth/login/')
+      .post('https://lifehelper-api.herokuapp.com/auth/login/')
       .send(credentials)
       .end(function (err, res) {
         //  If auth token is not returned, that means the user entered bad credentials.
